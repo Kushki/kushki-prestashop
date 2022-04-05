0176868a2bdedb5e75f9b068a284051b2538f6e9
David Absalon Cedeño <david.cedeno@kushkipagos.com>
2022-04-05 13:43:33 -0500

    hotfix version 3.0.2

--- kushkipagos/kushkipagos.php
+++ kushkipagos/kushkipagos.php
@@ -50,7 +50,7 @@ class Kushkipagos extends PaymentModule
     {
         $this->name = 'kushkipagos';
         $this->tab = 'payments_gateways';
-        $this->version = '3.0.1';
+        $this->version = '3.0.2';
         $this->author = 'Kushkipagos';
         $this->need_instance = 0;
         $this->display = 'view';
